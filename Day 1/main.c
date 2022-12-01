421df6dea6a80434b18d1270ed54abb7bc87dc62
Simone Mannarino <simone.mannarino@mail.polimi.it>
2022-12-01 12:51:09 +0100

    Day 1 second part completed

--- Day 1/main.c
+++ Day 1/main.c
@@ -7,13 +7,19 @@ int main(){
     char str[50];
     int x_read;
     int counter = 0;
-    int max_sum = -1;
+    int max_sum[3];
 
     pointer = fopen ("input.txt","r");
 
     if (pointer == NULL) {
         printf("File can't be opened \n");
     }
+
+    for (int i = 0; i < 3; i++)
+    {
+        max_sum[i] = -1;
+    }
+    
 
     while (fgets(str, sizeof(str), pointer) != NULL)
     {
@@ -22,12 +28,27 @@ int main(){
             counter += x_read;
         }
         else {
-            if(counter > max_sum) max_sum = counter;
+            if(counter > max_sum[2]){
+                if(counter > max_sum[1]){
+                    if(counter > max_sum[0]){
+                        max_sum[2] = max_sum[1];
+                        max_sum[1] = max_sum[0];
+                        max_sum[0] = counter;
+                    }
+                    else {
+                        max_sum[2] = max_sum[1];
+                        max_sum[1] = counter;
+                    }
+                }
+                else{
+                    max_sum[2] = counter;
+                }
+            } 
             counter = 0;
         }
     }
 
-    printf("%d",max_sum);
+    printf("%d\n",max_sum[0]+max_sum[1]+max_sum[2]);
     fclose(pointer);
     return 0;
 }
